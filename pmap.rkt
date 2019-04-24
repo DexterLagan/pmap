4351ab627d8624caa77c1fd3d64cc17239fb3587
DexterLagan <38226103+DexterLagan@users.noreply.github.com>
2019-04-24 14:04:53 +0200

    Initial commit

---
+++ pmap.rkt
@@ -0,0 +1,43 @@
+#lang racket
+(module+ test (require rackunit))
+
+;; generic concurrent map
+(define (pmap proc lst)
+  (define semaphore (make-semaphore 1))
+  (define lst-length (length lst))
+  (define results (make-vector lst-length #f))
+  (define indexes (for/list ((x (in-range lst-length))) x))
+  (define (pproc item index)
+    (thread (λ ()
+              (define output (proc item))
+              (semaphore-wait semaphore)
+              (vector-set! results index output)
+              (semaphore-post semaphore))))
+  (define threads (map pproc lst indexes))
+  (for-each thread-wait threads)
+  (vector->list results))
+; unit test
+(module+ test
+  (check-equal? (pmap string-upcase '("naice" "world" "buddy"))
+                '("NAICE" "WORLD" "BUDDY")))
+
+;; generic concurrent for-each
+(define (pfor-each proc lst)
+  (define semaphore (make-semaphore 1))
+  (define lst-length (length lst))
+  (define results (make-vector lst-length #f))
+  (define indexes (for/list ((x (in-range lst-length))) x))
+  (define (pproc item index)
+    (thread (λ ()
+              (define output (proc item))
+              (semaphore-wait semaphore)
+              (vector-set! results index output)
+              (semaphore-post semaphore))))
+  (define threads (map pproc lst indexes))
+  (for-each thread-wait threads))
+; unit test
+(module+ test
+  (check-equal? (pfor-each string-upcase '("naice" "world" "buddy"))
+                (void)))
+                
+; EOF
