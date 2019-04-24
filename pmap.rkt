372510a4ff7f89afed41736402005dfb0105e06e
DexterLagan <38226103+DexterLagan@users.noreply.github.com>
2019-04-24 17:36:26 +0200

    Added andpmap variant

--- pmap.rkt
+++ pmap.rkt
@@ -1,5 +1,6 @@
 #lang racket
 (module+ test (require rackunit))
+(provide pmap andpmap pfor-each)
 
 ;; generic concurrent map
 (define (pmap proc lst)
@@ -20,6 +21,15 @@
 (module+ test
   (check-equal? (pmap string-upcase '("naice" "world" "buddy"))
                 '("NAICE" "WORLD" "BUDDY")))
+
+;; generic concurrent map
+(define (andpmap proc lst)
+  (define results (pmap proc lst))
+  (if (member #f results) #f #t))
+; unit test
+(module+ test
+  (check-equal? (andpmap even? '(2 3 4)) #f)
+  (check-equal? (andpmap even? '(2 4 6)) #t))
 
 ;; generic concurrent for-each
 (define (pfor-each proc lst)
